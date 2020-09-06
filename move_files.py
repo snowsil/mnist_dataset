10423f233a75358d524c86327dad3cdd7419227c
snowsil <1419754775@qq.com>
2020-09-06 14:15:04 +0800

    submit

--- move_files.py
+++
@@ -1,9 +0,0 @@
-import shutil
-import os
-root_path = "/media/HD1/yuyue/"
-fp = open("test_video_list.txt","r")
-for line in fp:
-	list0 = [root_path,line.strip()]
-	src_path = "".join(list0)
-	shutil.move(src_path,"/media/HD1/yuyue/dataset/test_data")
-fp.close()
